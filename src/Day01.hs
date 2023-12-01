9dc4ba60fa5e1854fb6f22b102ca96cd6f624990
David Bernecker <david.bernecker@mvtec.com>
2023-12-01 13:27:41 +0100

    Simplify Day1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,145 +1,52 @@
 module Day01 (main) where
 
-import Control.Monad (void)
-import Data.Char (isLetter)
+import Data.List (isPrefixOf, tails)
 import System.Environment (getArgs)
-import Text.Parsec (alphaNum, digit, many1, parse, string, try, (<|>))
-import Text.Parsec.String (Parser)
 
-firstDigit :: String -> Char
-firstDigit s = head $ dropWhile isLetter s
-
-extractNumber :: String -> String
-extractNumber s = [a, b]
+extractNumbers :: (String -> Int) -> String -> Int
+extractNumbers condition s = a * 10 + b
   where
-    a = firstDigit s
-    b = firstDigit $ reverse s
+    t = tails s
+    a = takeFirst (/= 0) $ map condition t
+    b = takeFirst (/= 0) $ map condition $ reverse t
 
-toInt :: String -> Int
-toInt = read
+takeFirst :: (a -> Bool) -> [a] -> a
+takeFirst b l = head $ dropWhile (not . b) l
 
 part1 :: String -> String
-part1 s = show $ sum $ map (toInt . extractNumber) $ lines s
+part1 s = show $ sum $ map (extractNumbers matchDigit) $ lines s
 
-one :: Parser Int
-one = do
-  void $ string "one"
-  pure 1
+matchDigit :: String -> Int
+matchDigit s
+  | "1" `isPrefixOf` s = 1
+  | "2" `isPrefixOf` s = 2
+  | "3" `isPrefixOf` s = 3
+  | "4" `isPrefixOf` s = 4
+  | "5" `isPrefixOf` s = 5
+  | "6" `isPrefixOf` s = 6
+  | "7" `isPrefixOf` s = 7
+  | "8" `isPrefixOf` s = 8
+  | "9" `isPrefixOf` s = 9
+  | otherwise = 0
 
-two :: Parser Int
-two = do
-  void $ string "two"
-  pure 2
-
-three :: Parser Int
-three = do
-  void $ string "three"
-  pure 3
-
-four :: Parser Int
-four = do
-  void $ string "four"
-  pure 4
-
-five :: Parser Int
-five = do
-  void $ string "five"
-  pure 5
-
-six :: Parser Int
-six = do
-  void $ string "six"
-  pure 6
-
-seven :: Parser Int
-seven = do
-  void $ string "seven"
-  pure 7
-
-eight :: Parser Int
-eight = do
-  void $ string "eight"
-  pure 8
-
-nine :: Parser Int
-nine = do
-  void $ string "nine"
-  pure 9
-
-oneR :: Parser Int
-oneR = do
-  void $ string "eno"
-  pure 1
-
-twoR :: Parser Int
-twoR = do
-  void $ string "owt"
-  pure 2
-
-threeR :: Parser Int
-threeR = do
-  void $ string "eerht"
-  pure 3
-
-fourR :: Parser Int
-fourR = do
-  void $ string "ruof"
-  pure 4
-
-fiveR :: Parser Int
-fiveR = do
-  void $ string "evif"
-  pure 5
-
-sixR :: Parser Int
-sixR = do
-  void $ string "xis"
-  pure 6
-
-sevenR :: Parser Int
-sevenR = do
-  void $ string "neves"
-  pure 7
-
-eightR :: Parser Int
-eightR = do
-  void $ string "thgie"
-  pure 8
-
-nineR :: Parser Int
-nineR = do
-  void $ string "enin"
-  pure 9
-
-int :: Parser Int
-int = do
-  d <- digit
-  pure $ read [d]
-
-other :: Parser Int
-other = do
-  void alphaNum
-  pure 0
-
-calibrationValue :: Parser Int
-calibrationValue = int <|> try one <|> try two <|> try three <|> try four <|> try five <|> try six <|> try seven <|> try eight <|> try nine <|> other
-
-calibrationValueR :: Parser Int
-calibrationValueR = int <|> try oneR <|> try twoR <|> try threeR <|> try fourR <|> try fiveR <|> try sixR <|> try sevenR <|> try eightR <|> try nineR <|> other
-
-firstLast :: ([Int], [Int]) -> Int
-firstLast (l, r) = a * 10 + b
+matchNumber :: String -> Int
+matchNumber s
+  | 0 /= d = d
+  | "one" `isPrefixOf` s = 1
+  | "two" `isPrefixOf` s = 2
+  | "three" `isPrefixOf` s = 3
+  | "four" `isPrefixOf` s = 4
+  | "five" `isPrefixOf` s = 5
+  | "six" `isPrefixOf` s = 6
+  | "seven" `isPrefixOf` s = 7
+  | "eight" `isPrefixOf` s = 8
+  | "nine" `isPrefixOf` s = 9
+  | otherwise = 0
   where
-    a = head $ dropWhile (== 0) l
-    b = head $ dropWhile (== 0) r
-
-parseLine :: String -> ([Int], [Int])
-parseLine s = case (parse (many1 calibrationValue) "" s, parse (many1 calibrationValueR) "" (reverse s)) of
-  (Right f, Right l) -> (f, l)
-  _ -> ([], [])
+    d = matchDigit s
 
 part2 :: String -> String
-part2 s = show $ sum $ map (firstLast . parseLine) (lines s)
+part2 s = show $ sum $ map (extractNumbers matchNumber) $ lines s
 
 main :: IO ()
 main = do
